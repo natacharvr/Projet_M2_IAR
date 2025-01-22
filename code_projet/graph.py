c7c5acc8e44f2c1045d0c62f20a3f2abb0ab4e03
Natacha Rivière <riviere.natachamarie@gmail.com>
2025-01-22 17:54:30 +0100

    graph

--- code_projet/graph.py
+++ code_projet/graph.py
@@ -5,8 +5,11 @@ val_mid = [24, 30, 30, 29, 31, 31, 29, 34, 34, 17, 0]
 val_max = [15, 21, 30, 30, 32, 35, 30, 32, 32, 38, 17]
 
 plt.plot(val_min, label="IrP1")
-plt.plot(val_mid, label="IrP2")
 plt.plot(val_max, label="IrP3")
+plt.plot(val_mid, label="IrP2")
+plt.grid()
+plt.xlim(left=0)  # x-axis starts at 0
+plt.ylim(bottom=0)
 
 plt.xlabel('Distance (cm)')
 plt.ylabel('Nb messages received')
